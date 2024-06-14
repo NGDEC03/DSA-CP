3abcfdf2a338b447bd8efaa880d9e584e2ebdbd8
NGDEC03 <ngdec03@gmail.com>
2024-06-14 18:18:33 +0530

    Create arrayMax.java

---
+++ arrayMax.java
@@ -0,0 +1,10 @@
+public class arrayMax {
+    public static void main(String[] args) {
+        int[]arr={4,5,7,8,0,9};
+        int max=Integer.MIN_VALUE;
+        for(int i:arr){
+            if(i>max)max=i;
+        }
+        System.out.println("maximum element is "+max);
+    }
+}
