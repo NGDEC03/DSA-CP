c035f24013c4a867641237ffdd84f40fece81e77
NGDEC03 <ngdec03@gmail.com>
2024-06-14 18:23:04 +0530

    main

--- arrayMax.java
+++ arrayMax.java
@@ -1,6 +1,9 @@
 public class arrayMax {
     public static void main(String[] args) {
         int[]arr={4,5,7,8,0,9};
+
+
+
         int max=Integer.MIN_VALUE;
         for(int i:arr){
             if(i>max)max=i;
---
+++ arraySMax.java
@@ -0,0 +1,15 @@
+public class arraySMax {
+    public static void main(String[] args) {
+        int[]arr={4,5,7,8,0,1,-1};
+        int max=Integer.MIN_VALUE,sMax=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max){
+                sMax=max;
+                max=arr[i];
+                
+            }
+            else if(arr[i]>sMax && arr[i]!=max)sMax=arr[i];
+        }
+        System.out.println(max+"->"+sMax);
+    }
+}
